56f9b90ed768e1e558f989f9844577500d22ccf7
xuhao <runordie@qq.com>
2024-06-28 17:40:09 +0800

    feat:support launch vllm

--- src/launch_triton_server.py
+++ src/launch_triton_server.py
@@ -289,6 +289,128 @@ def get_engine_params(engine):
         js = json.load(fp)
         js['cfg_path'] = os.path.join(engine, 'config.json')
         return js
+
+def get_vllm_params(engine):
+    assert engine is not None
+    engine = Path(model)
+    assert engine.exists() and engine.is_dir()
+    param_path = engine / 'build_params.json'
+    assert param_path.exists()
+
+    with open(param_path, 'r') as fp:
+        js = json.load(fp)
+        js['cfg_path'] = os.path.join(engine, 'config.json')
+        return js
+
+def get_vllm_tp_size(devices):
+        total_cnt = torch.cuda.device_count()
+        assert total_cnt >= 1, f"can NOT get cuda device"
+        if devices:
+            devids = [int(s) for s in devices.split(",")]
+            assert (
+                len(devids) <= total_cnt
+            ), f"specified cuda devices {devices} more than total device {total_cnt}"
+            tp = len(devids)
+        else:
+            tp = total_cnt
+
+        print(f'vllm tp size {tp}')
+        return tp
+
+def get_vllm_cmd(tp_size, model, address, devices, image_size, feature_size, gpu_memory_utilization):
+    cmd = ""
+    if devices and len(devices) > 0:
+        cmd = "CUDA_VISIBLE_DEVICES=" + devices + " "
+
+    host = address.split(':')[0]
+    port = address.split(':')[1]
+    input_shape_list = [1,3,image_size[0],image_size[1]]
+    input_shape =  ','.join(str(x) for x in input_shape_list)
+
+    cmd += "python -m vllm.entrypoints.openai.api_server \
+                --model {} --host {} --port {} --tensor-parallel-size {} \
+                --image-input-type pixel_values --image-token-id 151646 \
+                --image-input-shape {} --image-feature-size {} \
+                --disable-log-requests --gpu-memory-utilization {} \
+                --chat-template /app/vllm/examples/template_chatml.jinja".format(
+                    model,host, port, tp_size, input_shape, feature_size, gpu_memory_utilization
+                )
+    # print(cmd)
+    return cmd
+
+def select_best_resolution(original_size: tuple, possible_resolutions: list) -> tuple:
+    """
+    Selects the best resolution from a list of possible resolutions based on the original size.
+
+    This is done by calculating the effective and wasted resolution for each possible resolution.
+
+    The best fit resolution is the one that maximizes the effective resolution and minimizes the wasted resolution.
+
+    Args:
+        original_size (tuple):
+            The original size of the image in the format (height, width).
+        possible_resolutions (list):
+            A list of possible resolutions in the format [(height1, width1), (height2, width2), ...].
+
+    Returns:
+        tuple: The best fit resolution in the format (height, width).
+    """
+    original_height, original_width = original_size
+    best_fit = None
+    max_effective_resolution = 0
+    min_wasted_resolution = float("inf")
+
+    for height, width in possible_resolutions:
+        scale = min(width / original_width, height / original_height)
+        downscaled_width, downscaled_height = int(original_width * scale), int(original_height * scale)
+        effective_resolution = min(downscaled_width * downscaled_height, original_width * original_height)
+        wasted_resolution = (width * height) - effective_resolution
+
+        if effective_resolution > max_effective_resolution or (
+            effective_resolution == max_effective_resolution and wasted_resolution < min_wasted_resolution
+        ):
+            max_effective_resolution = effective_resolution
+            min_wasted_resolution = wasted_resolution
+            best_fit = (height, width)
+
+    return best_fit
+
+def divide_to_patches(image_size, patch_size: int):
+    patch_cnt = 0
+    height, width = image_size
+    for i in range(0, height, patch_size):
+        for j in range(0, width, patch_size):
+            patch_cnt += 1
+            
+    return patch_cnt
+
+def get_llava_feature_size(model, image_size):
+    model = model.strip()
+    print(f'model {model}, image size {image_size}')
+    config_file_path = os.path.join(model, 'config.json')
+    assert os.path.exists(config_file_path) , f'{config_file_path} no config.json ?!'
+    with open('/models/llava-7b-unicom-qwen2-hd-800k/config.json', 'r') as fp:
+        js = json.load(fp)
+
+    image_grid_pinpoints = (
+        js['image_grid_pinpoints'] if js['image_grid_pinpoints'] is not None
+        else [[336, 672], [672, 336], [672, 672], [1008, 336], [336, 1008]]
+    )
+
+    patch_size = 336
+    preprocessor_config = os.path.join(model, 'preprocessor_config.json')
+    if os.path.exists(preprocessor_config):
+        with open(preprocessor_config, 'r') as fp:
+            js = json.load(fp)
+            patch_size = js['crop_size']
+
+
+    best_resolution = select_best_resolution(image_size, image_grid_pinpoints)
+    patches_cnt = divide_to_patches(best_resolution, patch_size)
+    print(f'patch cnt: {patches_cnt}')
+    feature_size = (1 + patches_cnt) * 576
+    return feature_size
+
 class Args:
     def __init__(self) -> None:
         pass
@@ -296,31 +418,6 @@ class Args:
         setattr(self, key, value)
 
 def main(args):
-    params = get_engine_params(args.engine)
-    model_name = params['name'].lower()
-    engine_cfg = params['cfg_path']
-
-    # check engine configuration
-    model = os.path.join(args.engine, 'model')
-    assert os.path.exists(model)
-
-    if not args.repo:
-        args.repo = "/app/all_models/inflight_batcher_llm"
-    args.repo = os.path.realpath(args.repo)
-    assert os.path.exists(args.repo), f"repo {args.repo} not exist"
-
-    # prepare triton parameters
-    triton = args.tritonserver if args.tritonserver else "/opt/tritonserver/bin/tritonserver"
-    triton_port = args.address.split(':')[1]
-    world, devids = get_world_size(engine_cfg, args.devices)
-
-    if args.gpu_mem_fraction is None or args.gpu_mem_fraction < 0.1:
-        args.gpu_mem_fraction  = decide_mem_fraction(world, devids, model_name)
-    model_path = build_triton_repo(
-        args.repo, args.engine, model, model_name, engine_cfg, args.schema, args.gpu_mem_fraction
-    )
-    cmd = get_cmd(world, triton, model_path, triton_port, args.devices)
-
     processes = {}
     # startup oaip if required, before triton startup
     oaip = args.oaip if args.oaip else "/app/oaip"
@@ -330,10 +427,49 @@ def main(args):
         print(">>> ", oaip)
         processes['oaip'] = subprocess.Popen(oaip, shell=True, preexec_fn=os.setsid)
 
-    print(">>> ", cmd)
-    processes['triton'] = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
+    if args.engine_name.lower() == "triton":
+        params = get_engine_params(args.engine)
+        model_name = params['name'].lower()
+        engine_cfg = params['cfg_path']
 
-    # this program monitor triton and oaip and any processes started by myself
+        # check engine configuration
+        model = os.path.join(args.engine, 'model')
+        assert os.path.exists(model)
+
+        if not args.repo:
+            args.repo = "/app/all_models/inflight_batcher_llm"
+        args.repo = os.path.realpath(args.repo)
+        assert os.path.exists(args.repo), f"repo {args.repo} not exist"
+
+        # prepare triton parameters
+        triton = args.tritonserver if args.tritonserver else "/opt/tritonserver/bin/tritonserver"
+        triton_port = args.address.split(':')[1]
+        world, devids = get_world_size(engine_cfg, args.devices)
+
+        if args.gpu_mem_fraction is None or args.gpu_mem_fraction < 0.1:
+            args.gpu_mem_fraction  = decide_mem_fraction(world, devids, model_name)
+        model_path = build_triton_repo(
+            args.repo, args.engine, model, model_name, engine_cfg, args.schema, args.gpu_mem_fraction
+        )
+        cmd = get_cmd(world, triton, model_path, triton_port, args.devices)
+        print(">>> ", cmd)
+        processes['triton'] = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
+    elif args.engine_name.lower() == "vllm":
+        #set timeout for vllm
+        if args.timeout is not None and args.timeout > 60: 
+            os.environ["VLLM_ENGINE_ITERATION_TIMEOUT_S"] = str(args.timeout)
+
+        feature_size = get_llava_feature_size(args.model, args.process_image_size)
+        tp_size = get_vllm_tp_size(args.devices)
+        gpu_mem_fraction = args.gpu_mem_fraction
+
+        cmd = get_vllm_cmd(tp_size, args.model, args.address, args.devices, args.process_image_size, feature_size, gpu_mem_fraction)
+        print(">>> ", cmd)
+        processes['vllm'] = subprocess.Popen(cmd, shell=True, preexec_fn=os.setsid)
+    else:
+        assert 0, "engine name must be triton or vllm"
+
+    # this program monitor triton/vllm and oaip and any processes started by myself
     # any exit subprocess will terminate everything
     exits = False
     try:
@@ -383,15 +519,34 @@ if __name__ == "__main__":
         js = json.load(fp)
     def setval(*a):
         set_value(js, args, *a)
-    setval("gpu_mem_fraction", None, "engine", "triton", "gpuMemFraction")
     setval("oaip", None, "sys", "oaip")
     setval("devices", None, "sys", "devices")
     setval("oaip", None, "sys", "oaip")
-    setval("schema", None, "engine", "triton", "schema")
-    setval("repo", None, "engine", "triton", "repo")
-    setval("engine", None, "engine", "triton", "engine")
-    setval("tritonserver", None, "engine", "triton", "tritonServer")
-    setval("address", None, "engine", "triton", "address")
+    setval("engine_name", None, "engine", "name")
+
+    engine_name = js['engine']['name']
+    if engine_name == "vllm":
+        #vllm args
+        print(f'select vllm engine')
+        setval("address", None, "engine", "vllm", "address")
+        setval("timeout", 60, "engine", "vllm", "timeout")
+        setval("model", None, "engine", "vllm", "model")
+        setval("gpu_mem_fraction", 0.9, "engine", "vllm", "gpuMemFraction")
+        setval("process_image_size", [1920, 1080], "engine", "vllm", "process_image_size")
+        assert args.model is not None and len(args.model) > 0
+    elif engine_name == "triton":
+        #triton args
+        print(f'select triton engine')
+        setval("schema", None, "engine", "triton", "schema")
+        setval("repo", None, "engine", "triton", "repo")
+        setval("engine", None, "engine", "triton", "engine")
+        setval("tritonserver", None, "engine", "triton", "tritonServer")
+        setval("address", None, "engine", "triton", "address")
+        setval("gpu_mem_fraction", None, "engine", "triton", "gpuMemFraction")
+    else:
+        assert 0, "only support triton & vllm engine"
+
     assert args.address is not None and len(args.address) > 0
+
     main(args)
     
